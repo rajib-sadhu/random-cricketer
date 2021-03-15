9eff52ab786a117ae2c087a44a662e9bbe43e184
rajib-sadhu <80105154+rajib-sadhu@users.noreply.github.com>
2021-03-15 18:04:36 +0530

    Add files via upload

---
+++ guess_gameJS.js
@@ -0,0 +1,26 @@
+   //guess game
+
+      //var guess=  parseInt(prompt("Enter no. from 1 to 5: "));
+
+      //alert("OK fine");
+      function guessFunction(){
+
+        var img=document.getElementById("imgId");
+        
+        var random_number = Math.floor(Math.random()*27)+1;
+
+        img.src="images/india/"+random_number+".png";
+
+    }
+
+    
+
+    function screenLoad(){
+
+        var preloader = document.getElementById("loading");
+    // window.addEventListener('load', function(){
+    // 	preloader.style.display = 'none';
+    // 	})
+        preloader.style.display = 'none';
+
+    }
