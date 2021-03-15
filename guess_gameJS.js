52e34b35f2bc97a1477cc6b08f8a51f7850a73f5
rajib-sadhu <80105154+rajib-sadhu@users.noreply.github.com>
2021-03-15 22:19:57 +0530

    Update guess_gameJS.js

--- guess_gameJS.js
+++ guess_gameJS.js
@@ -7,9 +7,24 @@
 
         var img=document.getElementById("imgId");
         
-        var random_number = Math.floor(Math.random()*27)+1;
+        var folder_random = Math.floor(Math.random()*3)+1;
 
-        img.src="images/india/"+random_number+".png";
+        var ind_random = Math.floor(Math.random()*27)+1;
+        var eng_random = Math.floor(Math.random()*4)+1;
+        var aus_random = Math.floor(Math.random()*5)+1;
+        
+
+        if(folder_random==1)
+        img.src="images/india/"+ind_random+".png";
+        if(folder_random==2)
+        img.src="images/england/"+eng_random+".png";
+        if(folder_random==3)
+        img.src="images/australia/"+aus_random+".png";
+
+         
+         
+         
+         
          var btn=document.getElementById("btn");
         btn.disabled=true;
           document.getElementById("span").innerHTML="you get once chance to see your player";
